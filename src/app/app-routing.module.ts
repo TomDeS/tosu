c70c9658370783d6521cf716e23c160bd7e11ab2
Tom De Sutter <Tom@PCTOSU.sofico.be>
2018-11-13 18:23:22 +0100

    basic navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { BankaccountComponent } from './components/bankaccount/bankaccount.component';
+import { PlayerComponent } from './components/player/player.component';
+import { ProductListComponent } from './components/shop/product-list/product-list.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'fries', component: ProductListComponent },
+  { path: 'home', component: PlayerComponent },
+  { path: 'tools', component: BankaccountComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
