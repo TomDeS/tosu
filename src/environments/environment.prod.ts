3e0c4cb8e74c98d1556020fceb4492e44aeb879d
Tom De Sutter <Tom@PCTOSU.sofico.be>
2018-12-20 18:39:56 +0100

    fix typo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true, 
+  firebase: {
+    apiKey: "AIzaSyDB-mVNIpEPnHGNrMUK5IbVPjHh_LE2CAU",
+    authDomain: "be-tosu.firebaseapp.com",
+    databaseURL: "https://be-tosu.firebaseio.com",
+    projectId: "be-tosu",
+    storageBucket: "be-tosu.appspot.com",
+    messagingSenderId: "347119726118"
+  }
 };
